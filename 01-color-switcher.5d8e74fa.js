d0e2e11bcae41fb9eb8f61b183275e97806d241d
Yevhen-Zhyvenkov <116755275+Yevhen-Zhyvenkov@users.noreply.github.com>
2023-04-20 19:47:57 +0000

    Deploying to gh-pages from @ Yevhen-Zhyvenkov/goit-js-hw-09@bb18694d0244eb5b77140890d768f56ee6d29554 🚀

---
+++ 01-color-switcher.5d8e74fa.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){timerId=setInterval((function(){t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.5d8e74fa.js.map
--- 01-color-switcher.e5240397.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.disabled=!0,document.body.style.backgroundColor=e}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(a)}));
-//# sourceMappingURL=01-color-switcher.e5240397.js.map
