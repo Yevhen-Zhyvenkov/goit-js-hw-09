dc5f8882699676941774ca0c4d777509a2585955
Yevhen-Zhyvenkov <zheka123730@gmail.com>
2023-04-21 16:31:44 +0300

    Виправлення

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 buttonStop.disabled = true;
-
+let timerId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
