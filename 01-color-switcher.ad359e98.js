319783ed4ee064a40d17a04e9d2994daa669fae3
Yevhen-Zhyvenkov <116755275+Yevhen-Zhyvenkov@users.noreply.github.com>
2023-04-21 13:32:15 +0000

    Deploying to gh-pages from @ Yevhen-Zhyvenkov/goit-js-hw-09@dc5f8882699676941774ca0c4d777509a2585955 🚀

--- 01-color-switcher.ad359e98.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){timerId=setInterval((()=>{t.disabled=!0,e.disabled=!1,document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.ad359e98.js.map
